7b9cfcdc6bc3b6c8e44bc2c25fcea9a99942ecf1
Sainan <63328889+Sainan@users.noreply.github.com>
2021-02-07 04:35:21 +0100

    Remove superfluous quotes around charset

--- src/ContentTextPlain.php
+++ src/ContentTextPlain.php
@@ -4,6 +4,6 @@ class ContentTextPlain extends ContentText
 {
 	function __construct(string $text, string $charset = "UTF-8", string $encoding = EncodingQuotedPrintable::class)
 	{
-		parent::__construct($text, "text/plain; charset=\"$charset\"", $encoding);
+		parent::__construct($text, "text/plain; charset=$charset", $encoding);
 	}
 }
